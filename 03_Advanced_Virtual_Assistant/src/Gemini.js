a614a095475a814e701513ec297225fa274a2a19
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-05-17 13:56:27 +0530

    Advance Virtual Assistant

---
+++ 03_Advanced_Virtual_Assistant/src/Gemini.js
@@ -0,0 +1,34 @@
+const apiKey = "AIzaSyD1qlSBtjzQQ4bp36OkPze9aFebaU2IPj4";
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 50,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  // console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
